cccee1792e065784b57759bcf7587fe43f1ec882
faizalshikalgar <85288970+faizalshikalgar@users.noreply.github.com>
2022-08-21 12:07:54 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerHub-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t faizalshikagar/demo-app:jma-2.1 .'
+        sh 'docker build -t faizalshikalgar/demo-app:jma-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push faizalshikalgar/demo-app:jma-2.1'
     }
